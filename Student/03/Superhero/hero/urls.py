bc449b9c10a9bb7df00e3a89341fd17200802b30
fvasq18 <156850573+fvasq18@users.noreply.github.com>
2024-09-23 22:32:44 -0600

    project 3

--- Student/03/Superhero/hero/urls.py
+++ Student/03/Superhero/hero/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SpidermanView, BatmanView, IndexView, SuperManView
+from .views import SpidermanView, BatmanView, IndexView, SuperManView, FlashView, BlackPantherView
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('spiderman', SpidermanView.as_view()),
     path('batman', BatmanView.as_view()),
     path('superman', SuperManView.as_view()),
+    path('flash',FlashView.as_view()),
+    path('blackpanther', BlackPantherView.as_view())
 ]
--- Student/03/Superhero/hero/views.py
+++ Student/03/Superhero/hero/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import TemplateView
 
 
+
 class IndexView(TemplateView):
-    template_name = 'heroes.html'
+    template_name = 'index.html'
+
 
 
 class SpidermanView(TemplateView):
@@ -12,7 +14,9 @@ class SpidermanView(TemplateView):
         return {
             'title': 'Spiderman',
             'body': 'My name is Peter Parker',
-            'image': '/static/images/spiderman.jpg'
+            'image': '/static/images/spiderman.jpg',
+            'strengths': 'Strengths: Super Strenght, Spider Sense, Sticky',
+            'weaknesses': 'Weaknesses: Young, and water '
         }
 
 
@@ -23,7 +27,9 @@ class BatmanView(TemplateView):
         return {
             'title': 'BatMan',
             'body': 'My name is Bruce Wayne',
-            'image': '/static/images/batman.jpg'
+            'image': '/static/images/batman.jpg',
+            'strengths':'Strenghts: Rich, Smart, Strong',
+            'weaknesses': 'Weaknesses: No Superpower, His parents'
         }
 
 
@@ -34,5 +40,32 @@ class SuperManView(TemplateView):
         return {
             'title': 'SuperMan',
             'body': 'My name is Clark Kent',
-            'image': '/static/images/superman.jpg'
+            'image': '/static/images/superman.jpg',
+            'strengths':'Strenghts: Super Stength, Flight, Laser',
+            'weaknesses': 'Weaknesses: Kryptonite'
+        }
+    
+class FlashView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Flash',
+            'body': 'My name is Barry Allen',
+            'image': '/static/images/flash.jpg',
+            'strengths':'Strenghts: Fast, Durable',
+            'weaknesses': 'Weaknesses: extreme heat and cold'
+        }
+    
+
+class BlackPantherView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Black Panther',
+            'body': 'My name is King TChalla',
+            'image': '/static/images/black_panther.jpg',
+            'strengths':'Strenghts: Rich, Smart, Vibranium',
+            'weaknesses': 'Weaknesses: Limited abilities, emotional attachments'
         }
